9dcdbc2123bb277015bb1764e9d97444513565cd
Abhi kr <abhinandkr56@gmail.com>
2020-06-30 17:46:02 +0530

    creted first interface

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,14 @@
+﻿using Core.Entity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProductByIdAsync(int Id);
+        Task<IReadOnlyList<Product>> GetProductsAsync();
+    }
+}
--- Ecommerse/Controllers/ProductController.cs
+++ Ecommerse/Controllers/ProductController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Core.Entity;
+using Core.Interfaces;
 using Infrastrcture.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -14,22 +15,22 @@ namespace Ecommerse.Controllers
     [ApiController]
     public class ProductController : ControllerBase
     {
-        private readonly StoreContext _context;
-        public ProductController(StoreContext context)
+        private readonly IProductRepository _repo;
+        public ProductController(IProductRepository repo)
         {
-            _context = context;
+            _repo = repo;
         }
         [HttpGet]
         public async Task<ActionResult<List<Product>>> GetProducts()
         {
-            var products = await _context.Products.ToListAsync();
+            var products = await _repo.GetProductsAsync();
             return Ok(products);
         }
         [HttpGet("{id}")]
         public async Task<ActionResult<Product>> GetProduct(int id)
         {
 
-            return await _context.Products.FindAsync(id);
+            return await _repo.GetProductByIdAsync(id);
         }
     }
 }
